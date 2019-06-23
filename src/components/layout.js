393aafd0cbd50e769b0d149ea3aae46f22c99da8
luke <lukevance90@gmail.com>
2019-06-23 10:49:58 -0600

    grommet test remove from header

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,24 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby";
+import { Grommet, Box } from 'grommet';
 
 import Header from "./header"
 import "./layout.css"
+
+const theme = {
+  global: {
+    color: {
+      'light': '#f5f5f5'
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,22 +39,28 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <Grommet theme={theme}>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            
+            <main>
+              <Box pad={{vertical: 'small', horizontal: 'medium'}} elevation='medium' background='light' direction='row'></Box>
+              {children}
+            </main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </Grommet>
       </>
     )}
   />

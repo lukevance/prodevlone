eb4c8b24bef83aad2fa1407505e100c9aa6aa00a
luke <lukevance90@gmail.com>
2019-07-05 16:07:52 -0600

    sidebar is working!

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,11 +5,11 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
-import { Grommet, Box, Collapsible, Text } from 'grommet';
-import { CaretNext } from 'grommet-icons';
+import { Grommet, Box, Collapsible, Text, Layer } from 'grommet';
+import { CaretNext, CaretPrevious } from 'grommet-icons';
 
 import Header from "./header";
 import "./layout.css";
@@ -27,55 +27,69 @@ const theme = {
   },
 };
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+// const Layout = ({ children }) => (
+class Layout extends Component {
+  state = {
+    showSideBar: false,
+  };
+
+  render() {
+    const {showSideBar} = this.state;
+    const {children} = this.props;
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-    render={data => (
-      <Grommet theme={theme}>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        
-        <Box direction='row' flex overflow={{horizontal: 'hidden'}}>
-          <Box direction='row' flex>
-            <Collapsible direction="horizontal" open={true}>
-              <Box
-                flex
-                width="medium"
-                background="light-2"
-                pad="small"
-                elevation="small"
-              >
-                <Text size="xlarge">Sidebar</Text>
+        `}
+        render={data => (
+          <Grommet theme={theme}>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            
+            <Box direction='row' flex overflow={{horizontal: 'hidden'}}>
+              <Box direction='row'>
+                <Collapsible direction="horizontal" open={showSideBar}>
+                  <Box
+                    flex
+                    width="medium"
+                    background="light-2"
+                    pad="small"
+                    elevation="small"
+                  >
+                    <Text size="xlarge">Sidebar</Text>
+                  </Box>
+                </Collapsible>
+                {showSideBar 
+                ? <CaretPrevious onClick={() => this.setState(prevState => ({ showSideBar: !prevState.showSideBar }))}/>
+                : <CaretNext onClick={() => this.setState(prevState => ({ showSideBar: !prevState.showSideBar }))}/>
+                }
               </Box>
-            </Collapsible>
-            <CaretNext onClick={()=> console.log('clicked the sidebar!')}/>
-            {/* <Button icon={<CaretNext />} onClick={() => this.setState(prevState => ({ showSideBar: !prevState.showSideBar }))}/> */}
-          </Box>
-          <Box 
-            flex 
-            align="center" 
-            justify="center" 
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
-            {children}
-          </Box>
-        </Box>
-      </Grommet>
-    )}
-  />
-)
+              <Box 
+                flex 
+                align="center" 
+                justify="center" 
+                style={{
+                  margin: `0 auto`,
+                  maxWidth: 960,
+                  padding: `0px 1.0875rem 1.45rem`,
+                  paddingTop: 0,
+                }}
+              >
+                {children}
+              </Box>
+            </Box>
+          </Grommet>
+        )}
+      />
+    );
+  }
+}
+  
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,

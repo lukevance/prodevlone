a6165df80ea09c9f8865128ce2736bce5ad548c8
luke <lukevance90@gmail.com>
2020-04-11 19:58:22 -0600

    minor updates

--- src/components/contact-section.js
+++ src/components/contact-section.js
@@ -12,18 +12,7 @@ const Contact = () => {
             <Heading id="contact">
                 Contact
             </Heading>
-            <Text size="large">I'm not a big fan of email. I'd much prefer you contact me on a platform where conversations are more natural. Like these:</Text>
-            <Box direction="row" margin={{top: "large"}}>
-                <Phone size="xlarge"/>
-                <Box pad={{left: "medium"}}>
-                    <Text size="xlarge">
-                        Text LukeBot at +1 (720)-575-2537
-                    </Text>
-                    <Text>
-                    You can use LukeBot to pass a message along to me, schedule a coffee meeting, or even get free fantasy football advice! 
-                    </Text>
-                </Box>
-            </Box>
+            <Text size="large">Email inboxes get pretty noisy. I'd much prefer you contact me on a platform where conversations are more natural. Like these:</Text>
             <Box direction="row" margin={{top: "large"}}>
                 <Slack size="xlarge"/>
                 <Box pad={{left: "medium"}}>
@@ -32,6 +21,17 @@ const Contact = () => {
                     </Text>
                     <Text>
                         The Denver Devs community is awesome, and they have a great Slack group. If you're not already on it, you can sign up here: {<a href="https://denverdevs.org" target="_blank">denverdevs.org</a>}
+                    </Text>
+                </Box>
+            </Box>
+            <Box direction="row" margin={{top: "large"}}>
+                <Phone size="xlarge"/>
+                <Box pad={{left: "medium"}}>
+                    <Text size="xlarge">
+                        Text LukeBot at +1 (720)-575-2537
+                    </Text>
+                    <Text>
+                    You can use LukeBot to pass a message along to me so we can text like humans do or meet for coffee. Or if you like talking to robots, you can even get free fantasy football advice!
                     </Text>
                 </Box>
             </Box>
